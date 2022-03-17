56161889a4d803caa2c67dd650d00de0a86425ea
Subhashree naik <subhashree.naik@gmail.com>
2022-03-17 22:41:31 +0530

    welcome to Address book using stream

---
+++ src/com/bridgelabz/assignment/Welcome.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.assignment;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to Address Book Application using Java Stream");
+
+	}
+
+}
